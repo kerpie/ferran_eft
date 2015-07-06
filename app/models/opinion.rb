a0ef94be085b326b19547c7657aa383ab7eababf
Kerry Thomas <k4torce@gmail.com>
2015-07-06 11:44:26 -0500

    new changes in validation

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -2,9 +2,11 @@ class Opinion < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 	#validates_attachment_content_type :image, :content_type => /^image\/(jpg|jpeg|pjpeg|png|x-png|gif)$/, :message => 'file type is not allowed (only jpeg/png/gif images)'  
-	validates_attachment :image,  	presence: true,
-                          			:message => 'file type is not allowed (only jpeg/png/gif images)' 
+	# validates_attachment :image,  	presence: true,
+ #                          			:message => 'file type is not allowed (only jpeg/png/gif images)' 
 
-    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+ #    validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png']
+
+ 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
